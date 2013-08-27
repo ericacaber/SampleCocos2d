a99c1da67a7c5cb0d4aad37a243b857fd88f2016
Erica Caber <ericacaber@klab.com>
2013-08-27 15:43:14 +0800

    Initial Commit

---
+++ Memory Game/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Memory Game
+//
+//  Created by Erica Caber on 8/27/13.
+//  Copyright Erica Caber 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
